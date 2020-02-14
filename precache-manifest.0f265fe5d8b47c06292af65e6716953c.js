e186f6d5655b83b77e95010c5371d0327f99f9cc
Hilarey <piatnytskiy.y.m@gmail.com>
2020-02-14 16:02:02 +0200

    Updates

---
+++ precache-manifest.0f265fe5d8b47c06292af65e6716953c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a48c43000a3e7d7eb2cac4729dedc83",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "ecb5d3a23a7d3561cb05",
+    "url": "/goit-react-hw-02-feedback/static/css/main.2a11a8f5.chunk.css"
+  },
+  {
+    "revision": "da72a78db441496e6079",
+    "url": "/goit-react-hw-02-feedback/static/js/2.49bd3ab6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/goit-react-hw-02-feedback/static/js/2.49bd3ab6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ecb5d3a23a7d3561cb05",
+    "url": "/goit-react-hw-02-feedback/static/js/main.2713b438.chunk.js"
+  },
+  {
+    "revision": "4c3d7c3ac1ad4fad6b67",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.77b613c8.js"
+  }
+]);
